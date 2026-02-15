5c1e4212840301305901ae434a4c2d55434a2483
Csaba Szy <szycsaba@gmail.com>
2026-02-15 16:51:07 +0100

    Seed cities

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\City;
 use Illuminate\Database\Seeder;
 
 class CitySeeder extends Seeder
@@ -12,6 +12,35 @@ class CitySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        City::create([
+            'name' => 'Athens',
+            'lat' => '37.9534',
+            'lon' => '23.7490',
+        ]);
+
+        City::create([
+            'name' => 'Budapest',
+            'lat' => '47.4980',
+            'lon' => '19.0399',
+        ]);
+
+        City::create([
+            'name' => 'Jakarta',
+            'lat' => '-6.2118',
+            'lon' => '106.8416',
+        ]);
+
+        City::create([
+            'name' => 'Rome',
+            'lat' => '41.8947',
+            'lon' => '12.4811',
+        ]);
+
+        City::create([
+            'name' => 'Wellington',
+            'lat' => '-41.2866',
+            'lon' => '174.7756',
+        ]);
+
     }
 }
